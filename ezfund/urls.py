97c72a8ca6373766daeebb93b7b98b6f1dd7b87a
caiyexiang <caiyexiang1@live.com>
2017-07-28 11:02:37 +0800

    用户系统的初步设置

--- ezfund/urls.py
+++ ezfund/urls.py
@@ -18,13 +18,21 @@ from django.contrib import admin
 from django.conf import settings
 from django.contrib.staticfiles import views
 from django.views.static import serve
-
+from django.contrib.auth import views as auth_views
+from fund import views as fund_views
 
 urlpatterns = [
     url(r'^fund/', include('fund.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^upload/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^login/', auth_views.login, {'template_name': 'account/sign_in.html'},name='login'),
+    url(r'logout_to_login/$', fund_views.logoutnlogin),
+    url(r'^password_change/$', auth_views.password_change, {'template_name': 'account/password_change_form.html','post_change_redirect': 
+'/fund/?pwd=1'}, name='password_change'),
+    url(r'^password_chang_done/$', auth_views.password_change_done,{'template_name': 'account/password_change_done.html'}, name='password_change_done'),
+    url(r'sign_up/$', fund_views.SignUpView, name='sign_up'),
 ]
+#password_change可以修改重定向到主页然后加一点JS特效提醒修改成功
 
 
 if settings.DEBUG:
--- fund/forms.py
+++ fund/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from .models import Fund
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 
 class FundForm(ModelForm):
     class Meta:
         model = Fund
         fields = '__all__'
+
+class UserForm(forms.Form):  
+    username = forms.CharField(label='用户名', max_length=30)
+    email = forms.EmailField(label='邮箱地址', max_length=50)
+    password = forms.CharField(label='密码', max_length=100,min_length=8,widget=forms.PasswordInput)
